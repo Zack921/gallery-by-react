eb664334a4200daba9e60cfe5602c9676e170497
Best921 <gzl407473490@163.com>
2018-02-28 22:05:04 +0800

    编写了画册主布局
    编写了画册主布局的html+css

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,6 @@ require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
 //获取图片相关数据
 let imageDatas = require('../data/imageDatas.json');
 
@@ -23,10 +21,10 @@ imageDatas = getImageURL(imageDatas);
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+    	<section className="stage">
+    		<section className="img-sec"></section>
+    		<nav className="controller-nav">sss</nav>
+    	</section>
     );
   }
 }
